18ca3c2f9db6390d648f377a2e1fe4b0105cbe96
teekae <tomkirk@outlook.com>
2025-11-08 23:15:57 +0000

    Allow skipping of tests
    This occurs if the postgres connection is not set

--- test/BaseFixture.cpp
+++ test/BaseFixture.cpp
@@ -4,8 +4,25 @@
 
 #include "BaseFixture.h"
 
-pqxx::connection BaseFixture::CreateConnection() {
-  const auto connString = std::getenv("POSTGRES_CONN");
+namespace {
+constexpr auto PGCONN_ENV_VAR = "POSTGRES_CONN";
+}
 
-  return pqxx::connection{connString};
+void BaseFixture::SetUp() {
+  const auto connString = std::getenv(PGCONN_ENV_VAR);
+  if (connString == nullptr) {
+    GTEST_SKIP()
+        << "Skipping test since no database connection string is set in "
+        << PGCONN_ENV_VAR << " environment variable.";
+  }
+
+  conn = std::make_unique<pqxx::connection>(connString);
+
+  tx = std::make_unique<pqxx::work>(*conn);
+}
+
+void BaseFixture::TearDown() {
+
+  // Don't commit to DB
+  tx->abort();
 }
